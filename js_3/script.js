0c37a25e9bcd4058b25af243df2f27ea9e29fbe1
arda <onur.arda.ozc@gmail.com>
2023-05-06 16:04:41 +0300

    javascript_3_variables
    Javascript entry level variables.

---
+++ js_3/script.js
@@ -0,0 +1,6 @@
+var maasJohn = 8500
+var maasJohny = 10000
+var enflasyonDegeri = 0.22  
+
+console.log(maasJohn + (maasJohn * enflasyonDegeri)) // maas john
+console.log(maasJohny + (maasJohny * enflasyonDegeri)) // maas johny
