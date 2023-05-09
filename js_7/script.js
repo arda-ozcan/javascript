c30bbbbb0fce9c1609e16a9fcb1336e1ed5c9ffb
arda <onur.arda.ozc@gmail.com>
2023-05-09 15:31:42 +0300

    if/else
    Condition structures.

---
+++ js_7/script.js
@@ -0,0 +1,25 @@
+let username = "onurarda";
+let passwaord = "1234";
+
+
+
+if(username == "onurarda")
+{
+    if(passwaord == "1234"){
+        console.log("Signed in to the app");
+    }
+    else{
+        console.log("Password is incorrect.");
+    }
+}
+else
+{
+    console.log("Username is incorrect.");
+}
+// if(!isLoggedin) 
+// {
+//     console.log("hi");
+// }
+
+
+
