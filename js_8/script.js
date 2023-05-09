f4dae5f14d4ed7cb4a016f967be44ee40fb2f4cb
arda <onur.arda.ozc@gmail.com>
2023-05-09 15:32:20 +0300

    if/else
    Conditional constructs using logical operators.

---
+++ js_8/script.js
@@ -0,0 +1,13 @@
+// yas >= 18
+// mezuniyet = "lise" ya da mezuniyet = "üniversite"
+
+let mezuniyet = "üniversite";
+let yas = 21;
+if (yas >= 18 && (mezuniyet == "lise"|| mezuniyet == "üniversite"))
+{
+    console.log("Ehliyet Alabilir");
+}
+else 
+{
+    console.log("Ehliyet Alamaz");
+}
